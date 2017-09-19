b29d9098000a33990821238e544338499e78a7e5
Elizabeth <epaul@princeton.edu>
2017-09-19 15:36:40 -0400

    Edits to save_nescin.

--- save_nescin.f90
+++ save_nescin.f90
@@ -7,17 +7,21 @@ subroutine save_nescin()
   implicit none
 
   integer :: save_nescin_mnmax, imn, m, n, itheta, izeta
-  integer :: iflag, mi, ni
+  integer :: iflag, mi, ni, ntheta_transform, nzetal_transform
   integer, dimension(:), allocatable :: save_nescin_xn, save_nescin_xm
   real(dp), dimension(:), allocatable :: save_nescin_rmnc, save_nescin_zmns
   real(dp), dimension(:,:), allocatable :: R_major_coil, z_coil
   real(dp) :: angle, dtransform
+  real(dp), dimension(:), allocatable :: theta_transform, zetal_transform
   character(13) :: nescin_outname = "nescin.offset"
   integer, parameter :: out_unit=20
   character(1) tab
   tab = char(9)
 
   save_nescin_mnmax = (save_nescin_ntor+1) + (save_nescin_mpol)*(2*save_nescin_ntor+1)
+
+  ntheta_transform = ntheta_coil*2
+  nzetal_transform = nzetal_coil*2
 
   allocate(save_nescin_rmnc(save_nescin_mnmax),stat=iflag)
   if (iflag .ne. 0) stop 'Allocation error!'
@@ -27,10 +31,21 @@ subroutine save_nescin()
   if (iflag .ne. 0) stop 'Allocation error!'
   allocate(save_nescin_xm(save_nescin_mnmax),stat=iflag)
   if (iflag .ne. 0) stop 'Allocation error!'
-  allocate(R_major_coil(ntheta_coil,nzetal_coil),stat=iflag)
+  allocate(R_major_coil(ntheta_transform,nzetal_transform),stat=iflag)
   if (iflag .ne. 0) stop 'Allocation error!'
-  allocate(z_coil(ntheta_coil,nzetal_coil),stat=iflag)
+  allocate(z_coil(ntheta_transform,nzetal_transform),stat=iflag)
   if (iflag .ne. 0) stop 'Allocation error!'
+  allocate(theta_transform(ntheta_transform),stat=iflag)
+  if (iflag .ne. 0) stop 'Allocation error!'
+  allocate(zetal_transform(nzetal_transform),stat=iflag)
+
+  do itheta=1,ntheta_transform
+    theta_transform(itheta) = (itheta-1.0_dp)/ntheta_transform
+  end do
+
+  do izeta = 1,nzetal_transform
+    zetal_transform(izeta) = (izeta-1.0_dp)/nzetal_transform
+  end do
 
   ! Initialize m = 0 modes
   imn = 1
@@ -51,15 +66,15 @@ subroutine save_nescin()
   R_major_coil = sqrt(r_coil(1,:,:)**2 + r_coil(2,:,:)**2)
   z_coil = r_coil(3,:,:)
 
-  dtransform = (1.0_dp)/(ntheta_coil*nzetal_coil)
+  dtransform = (1.0_dp)/(ntheta_transform*nzetal_transform)
   save_nescin_rmnc = 0
   save_nescin_zmns = 0
   do imn = 1,save_nescin_mnmax
     m = save_nescin_xm(imn)
     n = save_nescin_xn(imn)
-    do itheta = 1,ntheta_coil
-      do izeta = 1,nzetal_coil
-        angle = m*theta_coil(itheta) + nfp*n*zetal_coil(izeta)
+    do itheta = 1,ntheta_transform
+      do izeta = 1,nzetal_transform
+        angle = m*theta_transform(itheta) + nfp*n*zetal_transform(izeta)
         save_nescin_rmnc(imn) = save_nescin_rmnc(imn) + R_major_coil(itheta,izeta)*cos(angle)
         save_nescin_zmns(imn) = save_nescin_zmns(imn) + z_coil(itheta,izeta)*sin(angle)
       end do
