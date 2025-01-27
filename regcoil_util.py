815be642dd257dcd3ba368c2fa25e148e34b7ebb
Samuel Lazerson <lazersos@gmail.com>
2025-01-27 09:52:05 +0100

    Regcoil utility makes basic plots.

--- regcoil_util.py
+++ regcoil_util.py
@@ -4,14 +4,27 @@
 # Main routine
 if __name__=="__main__":
 	import sys
-	from regcoil import REGCOIL
+	from pyREGCOIL.regcoil import REGCOIL
+	from argparse import ArgumentParser
+	parser = ArgumentParser(description= 
+		'''Provides a utility for working with REGCOIL outputs.''')
+	parser.add_argument("-f", "--file", dest="file_name",
+		help="REGCOIL output filename.", default = None)
+	parser.add_argument("-p", "--plot", dest="lplots", action='store_true',
+		help="Produce some plots.", default = False)
+	args = parser.parse_args()
 	data=REGCOIL()
-	data.read_regcoil('regcoil_out.GIGA_v503_100.nc')
-	#data.plot_chisq()
-	#data.plot_current_potential(nlambda=0)
-	#data.plot_total_current_potential(nlambda=0)
-	#data.plot_current_density(nlambda=0)
-	#data.plot_bnormal_plasma()
-	#data.plot_bnormal_coil()
-	#data.plot_bnormal_total()
+	data.read_regcoil(args.file_name)
+	if args.lplots:
+		data.plot_chisq()
+		for n in range(data.nlambda):
+			data.plot_current_potential(nlambda=n)
+		for n in range(data.nlambda):
+			data.plot_total_current_potential(nlambda=n)
+		for n in range(data.nlambda):
+			data.plot_current_density(nlambda=n)
+		data.plot_bnormal_plasma()
+		data.plot_bnormal_coil()
+		for n in range(data.nlambda):
+			data.plot_bnormal_total(nlambda=n)
 	sys.exit(0)
